6d5b58ad38dad92a7f7f68a2b7aba73014032883
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-01-01 17:11:03 +0300

    add watcher and express tester

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,36 @@
 
 module.exports = require('grunto')(function(grunt) {
 
+	grunt.registerTask('default', [
+		'newer:jshint:all',
+		'nodeunit',
+		'watch'
+	]);
+
 	return {
-		jshint: {
+		nodeunit: {
+			all: [
+				'tests/*.js'
+			],
+			options: {
+			}
+		},
+		'jshint': {
 			options: grunt.file.readJSON('.jshintrc'),
 			all: [
 				'**/*.{js,json}',
 				'!node_modules/**/*.{js,json}',
 				'!lib-cov/**/*.{js,json}'
+			]
+		},
+		watch: {
+			files: [
+				'lib/**/*',
+				'tests/**/*'
+			],
+			tasks: [
+				'newer:jshint:all',
+				'nodeunit'
 			]
 		}
 	};
